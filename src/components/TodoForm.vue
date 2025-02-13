f15a91325186980100c5c998ba3654aa1bb92ae9
ReadyToDevNow <learntolearn0@gmail.com>
2025-02-13 10:11:39 +0100

    feat(): create a form component

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <form @submit.prevent="addTodo">
+    <input v-model="todo" placeholder="Ajouter une tâche" />
+    <button type="submit">Ajouter</button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useTodoListStore } from '@/stores/todolist'
+
+const todo = ref('')
+const store = useTodoListStore()
+
+function addTodo() {
+  if (todo.value.trim()) {
+    store.addTodo(todo.value)
+    todo.value = ''
+  }
+}
+</script>
+
+<style></style>
