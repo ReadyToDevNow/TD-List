6f72e2f1dfa618189dccd1a5dbb6a58421b2a2e7
ReadyToDevNow <learntolearn0@gmail.com>
2025-02-13 10:12:26 +0100

    feat(): create task list components

---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="todo in store.toDoList" :key="todo.id">
+      <span :class="{ completed: todo.completed }" @click="store.toggleCompleted(todo.id)">
+        {{ todo.item }}
+      </span>
+      <button @click="store.deleteTodo(todo.id)">Supprimer</button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { useTodoListStore } from '@/stores/todolist'
+
+const store = useTodoListStore()
+</script>
+
+<style scoped>
+.completed {
+  text-decoration: line-through;
+}
+</style>
