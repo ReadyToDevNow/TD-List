3f8e581502abc5a7da13f834ff3644e2093ae6cc
ReadyToDevNow <learntolearn0@gmail.com>
2025-02-19 17:55:43 +0100

    feat(): edit TodoList

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="todo in store.todoList" :key="todo.id">
-        <span :class="{ completed: todo.completed }" @click="store.toggleCompleted(todo.id)">
-          {{ todo.item + ' ' }}
-        </span>
-        <button @click="store.deleteTodo(todo.id)">Supprimer</button>
-      </li>
-    </ul>
+  <div class="min-h-screen bg-gray-200 flex flex-col justify-center items-center">
+    <h2 class="text-4xl font-bold text-blue-600">Liste de tâche</h2>
+    <span class="text-lg text-gray-600">Gérez vos tâches</span>
+    <section class="">
+      <ul>
+        <li v-for="todo in store.todoList" :key="todo.id">
+          <span :class="{ completed: todo.completed }" @click="store.toggleCompleted(todo.id)">
+            {{ todo.item + ' ' }}
+          </span>
+          <button
+            @click="store.deleteTodo(todo.id)"
+            class="bg-blue-500 border rounded text-white font-bold px-1 py-1 shadow-none mt-8 ml-6"
+          >
+            Supprimer
+          </button>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
